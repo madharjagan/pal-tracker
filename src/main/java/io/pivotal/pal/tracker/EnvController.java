2e4733c81731c693ac260d696d0ae4f411a09650
Jagan Kulala Sankar <jagan.sankar@gmail.com>
2018-08-13 16:19:56 -0600

    LAB2 DAY 1 Changes

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,41 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController {
+
+    String port;
+    String memory;
+    String instanceIndex;
+    String instanceAddr;
+
+
+    public EnvController(@Value("${PORT:NOT SET}") String port,
+                         @Value("${MEMORY_LIMIT:NOT SET}") String memory,
+                         @Value("${CF_INSTANCE_INDEX:NOT SET}")String instanceIndex,
+                         @Value("${CF_INSTANCE_ADDR:NOT SET}")String instanceAddr) {
+        this.port = port;
+        this.memory = memory;
+        this.instanceIndex = instanceIndex;
+        this.instanceAddr = instanceAddr;
+    }
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv(){
+        Map<String, String> env = new HashMap<>();
+
+        env.put("PORT", port);
+        env.put("MEMORY_LIMIT", memory);
+        env.put("CF_INSTANCE_INDEX", instanceIndex);
+        env.put("CF_INSTANCE_ADDR", instanceAddr);
+
+        return env;
+
+    }
+}
